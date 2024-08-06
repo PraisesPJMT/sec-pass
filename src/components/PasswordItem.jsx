2a03e0a7d401997f7adf5dd958759dc196c5a4c7
praisespjmt <praisesmusa@outlook.com>
2024-08-06 17:38:47 +0100

    `chore:` improve formating

--- src/components/PasswordItem.jsx
+++ src/components/PasswordItem.jsx
@@ -1,53 +1,65 @@
-import { useState, useRef } from 'react';
-import Key from '../assets/svgs/Key';
-import Show from '../assets/svgs/Show';
-import Hide from '../assets/svgs/Hide';
+import { useState, useRef } from "react";
+import PropTypes from "prop-types";
+
+import Key from "../assets/svgs/Key";
+import Hide from "../assets/svgs/Hide";
+import Show from "../assets/svgs/Show";
 
 const PasswordItem = ({
-	label = '',
-	value = '',
-	name = '',
-	handleChange = () => {},
-	staticType = false,
-	autoFocus = false,
-	pass = '',
+  label = "",
+  value = "",
+  name = "",
+  handleChange = () => {},
+  staticType = false,
+  autoFocus = false,
+  pass = "",
 }) => {
-	const [show, setShow] = useState(false);
-	const inputRef = useRef();
-	return (
-		<>
-			<div className='icon'>
-				<Key />
-			</div>
-			<div className='desc'>
-				<p className='label'>{label}</p>
-				{!staticType ? (
-					<p className='det'>{show ? pass : '*************'}</p>
-				) : (
-					<>
-						<input
-							ref={inputRef}
-							type={show ? 'text' : 'password'}
-							value={value}
-							name={name}
-							onChange={handleChange}
-							autoFocus={autoFocus}
-						/>
-					</>
-				)}
-				<button
-					type='button'
-					className='secondary-btn'
-					onClick={() => {
-						inputRef?.current?.focus();
-						setShow(!show);
-					}}
-				>
-					{!show ? <Show /> : <Hide />}
-				</button>
-			</div>
-		</>
-	);
+  const [show, setShow] = useState(false);
+  const inputRef = useRef();
+  return (
+    <>
+      <div className="icon">
+        <Key />
+      </div>
+      <div className="desc">
+        <p className="label">{label}</p>
+        {!staticType ? (
+          <p className="det">{show ? pass : "*************"}</p>
+        ) : (
+          <>
+            <input
+              ref={inputRef}
+              type={show ? "text" : "password"}
+              value={value}
+              name={name}
+              onChange={handleChange}
+              autoFocus={autoFocus}
+            />
+          </>
+        )}
+        <button
+          type="button"
+          className="secondary-btn"
+          onClick={() => {
+            inputRef?.current?.focus();
+            setShow(!show);
+          }}
+        >
+          {!show ? <Show /> : <Hide />}
+        </button>
+      </div>
+    </>
+  );
+};
+
+PasswordItem.propTypes = {
+  label: PropTypes.string,
+  value: PropTypes.string,
+  name: PropTypes.string,
+  handleChange: PropTypes.func,
+  staticType: PropTypes.bool,
+  autoFocus: PropTypes.bool,
+  pass: PropTypes.string,
 };
 
 export default PasswordItem;
