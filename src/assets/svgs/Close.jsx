1ae761a13e464315716f53a01c02f12bcdebccc8
praisespjmt <praisesmusa@outlook.com>
2024-08-09 08:38:30 +0100

    `chore:` add data attributes to svg components

--- src/assets/svgs/Close.jsx
+++ src/assets/svgs/Close.jsx
@@ -1,18 +1,19 @@
 const Close = () => {
-	return (
-		<svg
-			width='800px'
-			height='800px'
-			viewBox='0 0 20 20'
-			xmlns='http://www.w3.org/2000/svg'
-		>
-			<rect x='0' fill='none' width='20' height='20' />
+    return (
+        <svg
+            width="800px"
+            height="800px"
+            viewBox="0 0 20 20"
+            data-testid="close-svg"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <rect x="0" fill="none" width="20" height="20" />
 
-			<g>
-				<path d='M14.95 6.46L11.41 10l3.54 3.54-1.41 1.41L10 11.42l-3.53 3.53-1.42-1.42L8.58 10 5.05 6.47l1.42-1.42L10 8.58l3.54-3.53z' />
-			</g>
-		</svg>
-	);
+            <g>
+                <path d="M14.95 6.46L11.41 10l3.54 3.54-1.41 1.41L10 11.42l-3.53 3.53-1.42-1.42L8.58 10 5.05 6.47l1.42-1.42L10 8.58l3.54-3.53z" />
+            </g>
+        </svg>
+    );
 };
 
 export default Close;
